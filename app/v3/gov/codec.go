9fd181b04091d0146ba82bc483cf7bd80794d8c5
jiacheng <jiacheng@bianjie.ai>
2018-12-19 15:29:06 +0800

    update the jiacheng/refactor3

--- app/v3/gov/codec.go
+++ app/v3/gov/codec.go
@@ -2,29 +2,25 @@ package gov
 
 import (
 	"github.com/irisnet/irishub/codec"
-	"github.com/irisnet/irishub/modules/gov/params"
 )
 
 // Register concrete types on codec codec
 func RegisterCodec(cdc *codec.Codec) {
 
-	cdc.RegisterConcrete(MsgSubmitProposal{}, "cosmos-sdk/MsgSubmitProposal", nil)
-	cdc.RegisterConcrete(MsgSubmitTxTaxUsageProposal{}, "gov/MsgSubmitTxTaxUsageProposal", nil)
-	cdc.RegisterConcrete(MsgSubmitSoftwareUpgradeProposal{}, "gov/MsgSubmitSoftwareUpgradeProposal", nil)
-	cdc.RegisterConcrete(MsgDeposit{}, "cosmos-sdk/MsgDeposit", nil)
-	cdc.RegisterConcrete(MsgVote{}, "cosmos-sdk/MsgVote", nil)
+	cdc.RegisterConcrete(MsgSubmitProposal{}, "cosmos-sdk/MsgSubmitProposal3", nil)
+	cdc.RegisterConcrete(MsgSubmitTxTaxUsageProposal{}, "gov/MsgSubmitTxTaxUsageProposal3", nil)
+	cdc.RegisterConcrete(MsgSubmitSoftwareUpgradeProposal{}, "gov/MsgSubmitSoftwareUpgradeProposal3", nil)
+	cdc.RegisterConcrete(MsgDeposit{}, "cosmos-sdk/MsgDeposit3", nil)
+	cdc.RegisterConcrete(MsgVote{}, "cosmos-sdk/MsgVote3", nil)
 
 	cdc.RegisterInterface((*Proposal)(nil), nil)
-	cdc.RegisterConcrete(&TextProposal{}, "gov/TextProposal", nil)
+	cdc.RegisterConcrete(&TextProposal{}, "gov/TextProposal3", nil)
 
 	////////////////////  iris begin  ///////////////////////////
-	cdc.RegisterConcrete(&govparams.DepositProcedure{}, "cosmos-sdk/DepositProcedure", nil)
-	cdc.RegisterConcrete(&govparams.TallyingProcedure{}, "cosmos-sdk/TallyingProcedure", nil)
-	cdc.RegisterConcrete(&govparams.VotingProcedure{}, "cosmos-sdk/VotingProcedure", nil)
-	cdc.RegisterConcrete(&ParameterProposal{}, "gov/ParameterProposal", nil)
-	cdc.RegisterConcrete(&SoftwareUpgradeProposal{}, "gov/SoftwareUpgradeProposal", nil)
-	cdc.RegisterConcrete(&HaltProposal{}, "gov/TerminatorProposal", nil)
-	cdc.RegisterConcrete(&TaxUsageProposal{}, "gov/TaxUsageProposal", nil)
+	cdc.RegisterConcrete(&ParameterProposal{}, "gov/ParameterProposal3", nil)
+	cdc.RegisterConcrete(&SoftwareUpgradeProposal{}, "gov/SoftwareUpgradeProposal3", nil)
+	cdc.RegisterConcrete(&HaltProposal{}, "gov/TerminatorProposal3", nil)
+	cdc.RegisterConcrete(&TaxUsageProposal{}, "gov/TaxUsageProposal3", nil)
 	////////////////////  iris end  ///////////////////////////
 }
 
